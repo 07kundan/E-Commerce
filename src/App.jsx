b0e7fec0eb271da82efb80c4e908eee35f926324
07kundan <kundankumar28884@gmail.com>
2024-04-14 15:09:12 +0530

    fetching Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { fetchAllProductsData, fetchCategoryData } from "./Components/API";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [category, setCategory] = useState("smartphones");
+
+  useEffect(() => {
+    try {
+      const responseData = fetchAllProductsData();
+      console.log(responseData);
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+      const responseData = fetchCategoryData(category);
+      console.log(responseData);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <>
-      <h1 className="">hello</h1>
+      <h1 className="bg-slate-600">hello</h1>
     </>
   );
 }
---
+++ src/Components/API.js
@@ -0,0 +1,20 @@
+
+
+//  It'll give all products data
+export async function fetchAllProductsData() {
+    const response = await fetch('https://dummyjson.com/products');
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return response.json();
+  }
+
+
+//   It'll give products data of only given catogory
+  export async function fetchCategoryData(category) {
+    const response = await fetch(`https://dummyjson.com/products/category/${category}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return response.json();
+  }
