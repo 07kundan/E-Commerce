ddec8693ebea79875ae9e951044b3893840859c0
Mohit Kumar Pramanik <mohitpramanik548@gmail.com>
2024-04-18 08:04:04 +0530

    Created card component for product

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
-import { useEffect, useState } from "react";
-import { fetchAllProductsData, fetchCategoryData } from "./Components/API";
-import "./App.css";
+import React from "react";
+import Card from "./Components/Card";
 
-function App() {
-  const [category, setCategory] = useState("smartphones");
+const products = [
+    {
+        id: 1,
+        image: "https://naturali.co.in/cdn/shop/files/Artboard1_098b3e41-330b-439c-aa43-cea296ec8dfc.jpg?v=1710743204",
+        name: "Shampoo",
+        price: "100",
+    },
+    {
+        id: 2,
+        image: "https://www.letscurlup.com/cdn/shop/files/CurlHydratingConditioner.webp?v=1699440807",
+        name: "Conditioner",
+        price: "150",
+    },
+    {
+        id: 3,
+        image: "https://m.media-amazon.com/images/I/71FlO3G++cL._AC_UF1000,1000_QL80_DpWeblab_.jpg",
+        name: "Hair Oil",
+        price: "200",
+    },
+];
 
-  useEffect(() => {
-    try {
-      const responseData = fetchAllProductsData();
-      console.log(responseData);
-    } catch (error) {
-      console.log(error);
-    }
-
-    try {
-      const responseData = fetchCategoryData(category);
-      console.log(responseData);
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-
-  return (
-    <>
-      <h1 className="bg-slate-600">hello</h1>
-    </>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <div className="flex justify-around w-full">
+                {products.map((product) => (
+                    <Card key={product.id} product={product} />
+                ))}
+            </div>
+        </>
+    );
+};
 
 export default App;
