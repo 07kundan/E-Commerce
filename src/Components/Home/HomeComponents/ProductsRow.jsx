fe5978837515a90ad1361bf882f4162386163226
07kundan <kundankumar28884@gmail.com>
2024-04-20 18:36:26 +0530

    animation added

--- src/Components/Home/HomeComponents/ProductsRow.jsx
+++ src/Components/Home/HomeComponents/ProductsRow.jsx
@@ -1,43 +1,118 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import {
+  animate,
+  inView,
+  motion,
+  useAnimation,
+  useInView,
+} from "framer-motion";
 
 function ProductsRow() {
+  const ref = useRef(null);
+  const control = useAnimation();
+  const isInView = useInView(ref, { amount: 0.7 });
+
+  useEffect(() => {
+    if (isInView) {
+      // fire the animation
+      control.start("visible");
+    }
+
+    return () => control.stop();
+  }, [isInView]);
+
   return (
     <>
-      {/* <HeadingMarque value="trendings🔥" /> */}
+      <div
+        ref={ref}
+        className="h-screen w-screen m-auto my-16 flex justify-center items-center relative"
+      >
+        <h2 className="text-[#46C2CB] text-4xl font-bold ">Top Deals 🔥</h2>
 
-      <div className="h-[90vh] w-[90vw] m-auto my-16 text-3xl flex justify-center items-center relative">
-        <h2 className="bg-slate-600">Top Deals 🔥</h2>
-
-        <img
-          className="h-1/2 w-1/5 rounded-3xl bg-[https://cdn.dummyjson.com/product-images/1/1.jpg] absolute left-0 top-0"
-          src=""
-          alt=""
-        />
-        <img
-          className="h-1/2 w-1/5 rounded-3xl bg-purple-600 absolute left-4 -bottom-2"
-          src=""
-          alt=""
-        />
-        <img
-          className="h-1/2 w-1/5 rounded-3xl bg-purple-600 absolute right-0 top-0"
-          src=""
-          alt=""
-        />
-        <img
-          className="h-1/2 w-1/5 rounded-3xl bg-purple-600 absolute right-0 -bottom-4"
-          src=""
-          alt=""
-        />
-        <img
-          className="h-1/2 w-1/5 rounded-3xl bg-purple-600 absolute -top-5 rotate-90"
-          src=""
-          alt=""
-        />
-        <img
-          className="h-1/2 w-1/5 rounded-3xl bg-purple-600 absolute -bottom-4 rotate-90"
-          src=""
-          alt=""
-        />
+        <motion.span
+          variants={{
+            visible: { top: "10%" },
+          }}
+          animate={control}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className=" w-1/5 rounded-3xl bg-cover absolute"
+        >
+          <img
+            className=""
+            src="https://cdn.dummyjson.com/product-images/1/1.jpg"
+            alt=""
+          />
+        </motion.span>
+        <motion.span
+          variants={{
+            visible: { top: "73%" },
+          }}
+          animate={control}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="w-1/5 rounded-3xl absolute"
+        >
+          <img
+            className=""
+            src="https://cdn.dummyjson.com/product-images/1/1.jpg"
+            alt=""
+          />
+        </motion.span>
+        <motion.span
+          variants={{
+            visible: { top: "25%", left: "10%" },
+          }}
+          animate={control}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="w-1/5 rounded-3xl absolute"
+        >
+          <img
+            className=""
+            src="https://cdn.dummyjson.com/product-images/1/1.jpg"
+            alt=""
+          />
+        </motion.span>
+        <motion.span
+          variants={{
+            visible: { top: "65%", left: "10%" },
+          }}
+          animate={control}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="w-1/5 rounded-3xl absolute"
+        >
+          <img
+            className=""
+            src="https://cdn.dummyjson.com/product-images/1/1.jpg"
+            alt=""
+          />
+        </motion.span>
+        <motion.span
+          variants={{
+            visible: { top: "25%", left: "70%" },
+          }}
+          animate={control}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="w-1/5 rounded-3xl absolute"
+        >
+          <img
+            className=""
+            src="https://cdn.dummyjson.com/product-images/1/1.jpg"
+            alt=""
+          />
+        </motion.span>
+        <motion.span
+          variants={{
+            visible: { top: "65%", left: "70%" },
+          }}
+          animate={control}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="w-1/5 rounded-3xl absolute"
+        >
+          <img
+            className=""
+            src="https://cdn.dummyjson.com/product-images/1/1.jpg"
+            alt=""
+          />
+        </motion.span>
       </div>
     </>
   );
