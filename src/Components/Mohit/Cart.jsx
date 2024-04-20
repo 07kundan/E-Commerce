f059c064fc21c4b8de7a38ec1c575cf188b36fc8
Mohit Kumar Pramanik <mohitpramanik548@gmail.com>
2024-04-20 17:42:29 +0530

    Updated login page

--- src/Components/Mohit/Cart.jsx
+++ src/Components/Mohit/Cart.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoMdCart } from "react-icons/io";
 
 function Cart() {
-    const products = [
+    const [cartProduct, setCartProduct] = useState([
         {
             id: 1,
             name: "Product 1",
@@ -26,117 +27,142 @@ function Cart() {
             rating: 4.7,
             quantity: 1,
         },
-    ];
+    ]);
 
-    const total = products.reduce(
+    const total = cartProduct.reduce(
         (acc, product) => acc + product.price * product.quantity,
         0
     );
 
     const increaseQuantity = (productId) => {
-        const updatedProducts = products.map((product) => {
+        const updatedcartProduct = cartProduct.map((product) => {
             if (product.id === productId) {
                 return { ...product, quantity: product.quantity + 1 };
             }
             return product;
         });
-        // Update state or do other actions with updatedProducts
+        setCartProduct(updatedcartProduct);
     };
 
     const decreaseQuantity = (productId) => {
-        const updatedProducts = products.map((product) => {
+        const updatedcartProduct = cartProduct.map((product) => {
             if (product.id === productId && product.quantity > 1) {
                 return { ...product, quantity: product.quantity - 1 };
             }
             return product;
         });
-        // Update state or do other actions with updatedProducts
+        setCartProduct(updatedcartProduct);
     };
 
     const removeItem = (productId) => {
-        const updatedProducts = products.filter(
+        const updatedcartProduct = cartProduct.filter(
             (product) => product.id !== productId
         );
-        // Update state or do other actions with updatedProducts
+        setCartProduct(updatedcartProduct);
     };
 
     return (
         <div className="container mx-auto p-8">
-            <h1 className="text-3xl font-semibold mb-8">Your Cart</h1>
-            <div className="bg-white shadow-md rounded-lg overflow-hidden">
-                <table className="w-full">
-                    <thead className="bg-gray-100 border-b">
-                        <tr>
-                            {[
-                                "Product",
-                                "Price",
-                                "Rating",
-                                "Quantity",
-                                "Remove",
-                            ].map((item) => (
-                                <th className="py-3 px-4 text-left font-medium">
-                                    {item}
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {products.map((product) => (
-                            <tr key={product.id} className="border-b">
-                                <td className="py-4 px-6">
-                                    <img
-                                        src={product.image}
-                                        alt={product.name}
-                                        className="w-12 h-12 mr-4 object-cover object-center rounded-md inline-block"
-                                    />
-                                    {product.name}
-                                </td>
-                                <td className="py-4 px-6">${product.price}</td>
-                                <td className="py-4 px-6">
-                                    {product.rating.toFixed(1)}
-                                </td>
-                                <td className="py-4 px-6">
-                                    <button
-                                        className="bg-blue-500 text-white px-2 py-1 rounded-md"
-                                        onClick={() =>
-                                            increaseQuantity(product.id)
-                                        }
+            <h1 className="text-4xl font-semibold mb-8 text-white">
+                Your Cart
+            </h1>
+            {cartProduct.length == 0 ? (
+                <p className="text-white text-center text-3xl font-light flex items-center gap-6 justify-center">
+                    <IoMdCart /> Cart is empty
+                </p>
+            ) : (
+                <>
+                    <div className="bg-[floralwhite] shadow-md rounded-lg overflow-hidden">
+                        <table className="w-full">
+                            <thead className="bg-blue-500 border-b">
+                                <tr>
+                                    {[
+                                        "Product",
+                                        "Price",
+                                        "Rating",
+                                        "Quantity",
+                                        "Remove",
+                                    ].map((item) => (
+                                        <th className="py-3 px-4 text-left font-medium">
+                                            {item}
+                                        </th>
+                                    ))}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {cartProduct.map((product) => (
+                                    <tr key={product.id} className="border-b">
+                                        <td className="py-4 px-6">
+                                            <img
+                                                src={product.image}
+                                                alt={product.name}
+                                                className="w-12 h-12 mr-4 object-cover object-center rounded-md inline-block"
+                                            />
+                                            {product.name}
+                                        </td>
+                                        <td className="py-4 px-6">
+                                            Rs. {product.price}
+                                        </td>
+                                        <td className="py-4 px-6">
+                                            {product.rating.toFixed(1)}
+                                        </td>
+                                        <td className="py-4 px-6">
+                                            <button
+                                                className="bg-blue-500 text-white px-2 py-1 rounded-md"
+                                                onClick={() =>
+                                                    increaseQuantity(product.id)
+                                                }
+                                            >
+                                                +
+                                            </button>
+                                            <span className="mx-2">
+                                                {product.quantity}
+                                            </span>
+                                            <button
+                                                className="bg-red-500 text-white px-2 py-1 rounded-md"
+                                                onClick={() =>
+                                                    decreaseQuantity(product.id)
+                                                }
+                                            >
+                                                -
+                                            </button>
+                                        </td>
+                                        <td className="py-4 px-6">
+                                            <button
+                                                className="bg-red-500 text-white px-2 py-1 rounded-md"
+                                                onClick={() =>
+                                                    removeItem(product.id)
+                                                }
+                                            >
+                                                Remove
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                                <tr className="border-t">
+                                    <td
+                                        colSpan="3"
+                                        className="py-4 px-6 font-semibold"
                                     >
-                                        +
-                                    </button>
-                                    <span className="mx-2">
-                                        {product.quantity}
-                                    </span>
-                                    <button
-                                        className="bg-red-500 text-white px-2 py-1 rounded-md"
-                                        onClick={() =>
-                                            decreaseQuantity(product.id)
-                                        }
+                                        Total
+                                    </td>
+                                    <td
+                                        colSpan="2"
+                                        className="py-4 px-6 font-semibold"
                                     >
-                                        -
-                                    </button>
-                                </td>
-                                <td className="py-4 px-6">
-                                    <button
-                                        className="bg-red-500 text-white px-2 py-1 rounded-md"
-                                        onClick={() => removeItem(product.id)}
-                                    >
-                                        Remove
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
-                        <tr className="border-t">
-                            <td colSpan="3" className="py-4 px-6 font-semibold">
-                                Total
-                            </td>
-                            <td colSpan="2" className="py-4 px-6 font-semibold">
-                                ${total}
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                                        Rs. {total}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div className="bg-yellow-300 rounded-md mt-8 p-3 text-center">
+                        Proceed to Buy ({cartProduct.length}{" "}
+                        {cartProduct.length == 1 ? "item" : "items"})
+                    </div>
+                </>
+            )}
         </div>
     );
 }
--- src/Components/Mohit/LoginPage.jsx
+++ src/Components/Mohit/LoginPage.jsx
@@ -5,32 +5,48 @@ function LoginPage() {
 
     return (
         <div className="flex justify-center items-center h-screen w-screen">
-            <div className="flex flex-col border-2 border-black p-5 pt-0 rounded-xl h-max w-[500px]">
+            <div className="flex flex-col border-2 bg-slate-300 border-black p-5 pt-0 rounded-xl h-max w-[500px]">
                 <h2 className="text-4xl font-bold text-center m-6">Login</h2>
                 <div>
                     <form
-                        action=""
-                        className="flex flex-col justify-around gap-2"
+                        className="flex flex-col justify-around"
                         onSubmit={(e) => {
                             e.preventDefault();
                         }}
                     >
-                        <input
-                            type="text"
-                            name="username"
-                            id="usename"
-                            placeholder="Enter your Name"
-                        />
-                        <div className="w-full relative flex items-center">
+                        {/* Username  */}
+                        <div className="w-full relative">
                             <input
-                                type={isHidePassword ? "text" : "password"}
-                                name="password"
-                                id="password"
-                                placeholder="Enter your Password"
-                                className="w-full"
+                                type="text"
+                                name="username"
+                                id="usename"
+                                placeholder=""
+                                autoComplete="off"
                             />
+                            <label htmlFor="username" className="input-label">
+                                UserName
+                            </label>
+                        </div>
+                        {/* password  */}
+                        <div className="w-full relative flex items-center mt-10">
+                            <div className="w-full absolute bottom-0 ">
+                                <input
+                                    type={isHidePassword ? "text" : "password"}
+                                    name="password"
+                                    id="password"
+                                    placeholder=""
+                                    className="w-full"
+                                    autoComplete="off"
+                                />
+                                <label
+                                    htmlFor="password"
+                                    className="input-label"
+                                >
+                                    Password
+                                </label>
+                            </div>
                             <button
-                                className="absolute right-4 text-2xl"
+                                className="absolute right-4 bottom-2 text-2xl"
                                 onClick={(e) => {
                                     e.preventDefault();
                                     setIsHidePassword(!isHidePassword);
@@ -43,9 +59,33 @@ function LoginPage() {
                                 )}
                             </button>
                         </div>
+                        {/* forgot password and sign up section  */}
+                        <div className="flex flex-col gap-4 h-max py-4">
+                            <div className="h-max flex justify-between p-1">
+                                <div className="flex gap-2">
+                                    <input
+                                        type="checkbox"
+                                        name="remember"
+                                        id="remember"
+                                    />
+                                    <label htmlFor="remember">
+                                        Remember me
+                                    </label>
+                                </div>
+                                <div className="font-medium text-[#2e2ef6]">
+                                    Forgot Password?
+                                </div>
+                            </div>
+                            <p className="text-center">
+                                Don't have an account?{" "}
+                                <span className="font-semibold text-[#2e2ef6]">
+                                    Sign Up
+                                </span>
+                            </p>
+                        </div>
 
                         <button className="bg-blue-600 text-white font-bold mt-3 p-3 text-lg rounded-md active:bg-cyan-600 hover:bg-blue-700">
-                            Submit
+                            Login
                         </button>
                     </form>
                 </div>
