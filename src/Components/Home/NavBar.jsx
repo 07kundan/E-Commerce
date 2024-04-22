f98ac8b75a70f3e13c3eb1e034547581d33ed7cb
07kundan <kundankumar28884@gmail.com>
2024-04-22 19:10:59 +0530

    Navbar collision fixes

--- src/Components/Home/NavBar.jsx
+++ src/Components/Home/NavBar.jsx
@@ -11,38 +11,38 @@ import { FiPackage } from "react-icons/fi";
 import { motion } from "framer-motion";
 
 const NavItems = [
-    { path: "", name: "Home", icon: <SiHomeassistantcommunitystore /> },
-    { path: "Login", name: "Login", icon: <CgLogIn /> },
-    { path: "cart", name: "Cart", icon: <IoMdCart /> },
+  { path: "", name: "Home", icon: <SiHomeassistantcommunitystore /> },
+  { path: "Login", name: "Login", icon: <CgLogIn /> },
+  { path: "cart", name: "Cart", icon: <IoMdCart /> },
 ];
 
 const MenuList = [
-    {
-        icon: <CgProfile />,
-        name: "Profile",
-        path: "profile",
-    },
-    {
-        icon: <FaHeart />,
-        name: "Wishlist",
-        path: "wishlist",
-    },
-    {
-        icon: <FiPackage />,
-        name: "Order",
-        path: "",
-    },
-    {
-        icon: <FaGift />,
-        name: "Rewards",
-        path: "rewards",
-    },
+  {
+    icon: <CgProfile />,
+    name: "Profile",
+    path: "profile",
+  },
+  {
+    icon: <FaHeart />,
+    name: "Wishlist",
+    path: "wishlist",
+  },
+  {
+    icon: <FiPackage />,
+    name: "Order",
+    path: "",
+  },
+  {
+    icon: <FaGift />,
+    name: "Rewards",
+    path: "rewards",
+  },
 ];
 // Navbar
 function NavBar() {
-    const [HamburgerIsActive, setHambuegeIsActive] = useState(false);
-    const [searchIsActive, setSearchIsActive] = useState(false);
-    const [menuIsActive, setMenuIsActive] = useState(false);
+  const [HamburgerIsActive, setHambuegeIsActive] = useState(false);
+  const [searchIsActive, setSearchIsActive] = useState(false);
+  const [menuIsActive, setMenuIsActive] = useState(false);
 
   return (
     <>
@@ -63,22 +63,56 @@ function NavBar() {
 
         {/* Hamburger for smaller screen */}
         <>
-            <div className="w-screen p-3 px-8 text-[#655ee6] flex m-auto justify-between items-center text-2xl bg-[#453C97] relative z-10 lg:p-2 lg:pl-10  lg:rounded-2xl lg:my-2 lg:w-[83vw] ">
-                {/* Name */}
-                <motion.span
-                    variants={{
-                        initial: { x: -90, opacity: 0.5 },
-                        end: { x: 0, opacity: 1 },
-                    }}
-                    initial="initial"
-                    animate="end"
-                    transition={{ delay: 0.5, duration: 1, ease: "backInOut" }}
-                    className="text-2xl font-bold lg:w-[20%] lg:pl-7 text-indigo-950"
-                >
-                    ZED-kart
-                </motion.span>
+          {window.innerWidth < 1024 && (
+            <button
+              className="text-4xl"
+              onClick={() => setHambuegeIsActive(true)}
+            >
+              <MdMenu />
+            </button>
+          )}
 
-                {/* Hamburger for smaller screen */}
+          {/* Navlist if HamburgerIsActive */}
+          {HamburgerIsActive && (
+            <>
+              <div className="w-full h-[100vh] p-4 absolute top-0 right-0 text-2xl z-20 bg-white">
+                <button
+                  className="text-3xl float-end p-4 "
+                  onClick={() => setHambuegeIsActive(false)}
+                >
+                  <IoIosCloseCircle />
+                </button>
+                {/* routes */}
+                <div className="h-full p-12 flex flex-col gap-4  ">
+                  {NavItems.map((item) => (
+                    <NavLink
+                      onClick={() => setHambuegeIsActive(false)}
+                      to={`/${item.path}`}
+                      key={item.name}
+                      className={({ isActive }) =>
+                        `flex items-center gap-2 ${
+                          isActive ? "text-[#c79e3d]" : "text-[#FFD369]"
+                        }`
+                      }
+                    >
+                      <span className="text-2xl ">{item.icon}</span>
+                      <span>{item.name}</span>
+                    </NavLink>
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
+        </>
+        {/* ---------------------------- */}
+        {/* ---------------------------- */}
+
+        {/*  For Big Screen */}
+        {window.innerWidth > 1024 && (
+          <div className="text-lg flex justify-around gap-10 p-1 px-3 pr-12">
+            {/* search icon and search input if searchIsActive is true */}
+            <div className="flex gap-3 items-center">
+              {searchIsActive && (
                 <>
                   <motion.input
                     variants={{
@@ -119,9 +153,9 @@ function NavBar() {
                 to={`/${item.path}`}
                 key={item.name}
                 className={({ isActive }) =>
-                  ` ${
+                  `flex items-center gap-2 ${
                     isActive
-                      ? "text-[#46C2CB] underline underline-offset-4 "
+                      ? "text-[#37a6ae] underline underline-offset-2"
                       : "text-[#46C2CB]"
                   }`
                 }
@@ -204,13 +238,13 @@ function NavBar() {
           >
             <ul>
               {MenuList.map((item) => (
-                <a
-                  href="#"
+                <NavLink
+                  to={item.path}
                   className="flex gap-2 items-center py-4 px-4 hover:underline text-xl text-[#46C2CB]"
                 >
                   <li>{item.icon}</li>
                   <li>{item.name}</li>
-                </a>
+                </NavLink>
               ))}
             </ul>
           </motion.div>
