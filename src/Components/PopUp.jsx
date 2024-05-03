5b1196e4c647facb8d01f3df3a9e3fd9b22c80b1
07kundan <kundankumar28884@gmail.com>
2024-05-03 12:53:33 +0530

    popUp bug fixes

--- src/Components/PopUp.jsx
+++ src/Components/PopUp.jsx
@@ -4,45 +4,19 @@ import { motion } from "framer-motion";
 
 function PopUp() {
   const [popUp, setPopUp] = useState(false);
-  const [isLoggedInPopUp, setIsLoggedInPopUp] = useState();
   const { authenticationState, current } = useUser();
 
   // for popUp authentication State
   useEffect(() => {
-    if (authenticationState) {
-      setPopUp(true);
-      const interval = setTimeout(() => {
-        setPopUp(false);
-      }, 4000);
-      return () => clearInterval(interval);
-    }
+    setPopUp(true);
+    const interval = setTimeout(() => {
+      setPopUp(false);
+    }, 4000);
+    return () => clearInterval(interval);
   }, [authenticationState]);
-
-  useEffect(() => {
-    if (!current) {
-      setIsLoggedInPopUp(true);
-      const interval = setTimeout(() => {
-        setIsLoggedInPopUp(false);
-      }, 4000);
-      return () => clearInterval(interval);
-    }
-  }, []);
 
   return (
     <div className=" w-full absolute top-[4.3rem] flex justify-center">
-      {isLoggedInPopUp && (
-        <motion.div
-          initial={{
-            opacity: 0,
-            y: -100,
-          }}
-          animate={{ y: 0, opacity: 1 }}
-          className="flex justify-center w-fit p-3 px-8 text-xl rounded-2xl border border-b-4 border-green-500 bg-slate-700/80 "
-        >
-          Login to explore more
-        </motion.div>
-      )}
-
       {popUp && (
         <motion.div
           initial={{
@@ -52,7 +26,11 @@ function PopUp() {
           animate={{ y: 0, opacity: 1 }}
           className="flex justify-center p-3 px-8  text-xl rounded-2xl border border-b-4 border-green-500 z-50 bg-slate-700/80 "
         >
-          {authenticationState} successfully
+          {authenticationState ? (
+            <span>{authenticationState} successfully</span>
+          ) : (
+            <span>Login to explore more</span>
+          )}
         </motion.div>
       )}
     </div>
