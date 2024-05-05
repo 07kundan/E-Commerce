f4d06378d8de503165e7cf07a02be0e823faec9d
07kundan <kundankumar28884@gmail.com>
2024-05-05 21:20:34 +0530

    deals page responsiveness

--- src/Components/Home/HomeComponents/DealsPages/DealsPage.jsx
+++ src/Components/Home/HomeComponents/DealsPages/DealsPage.jsx
@@ -7,10 +7,11 @@ function DealsPage({ Id, emoji }) {
 
   return (
     <>
+      <hr />
       <div
         id={Id}
         ref={ref}
-        className=" h-screen w-screen m-auto flex justify-center items-center relative"
+        className="h-screen w-screen flex flex-col-reverse items-center py-10 relative  lg:flex-row lg:justify-center lg:p-0"
       >
         <h2 className=" text-4xl font-bold ">
           {Id.toUpperCase()} {"DEALS"} {emoji}
@@ -18,39 +19,51 @@ function DealsPage({ Id, emoji }) {
 
         <ItemCard
           reference={ref}
-          top={"4px"}
+          bottom={window.innerWidth > 1024 ? "65%" : "71%"}
+          left={window.innerWidth > 1024 ? "" : "5%"}
           ImageUrl={ImageCollection[Id].arr[0]}
+          emoji={emoji}
         />
+
         <ItemCard
           reference={ref}
-          bottom={"4px"}
-          ImageUrl={ImageCollection[Id].arr[1]}
-        />
-        <ItemCard
-          reference={ref}
-          top={"65%"}
-          left={"70%"}
-          ImageUrl={ImageCollection[Id].arr[2]}
-        />
-        <ItemCard
-          reference={ref}
-          top={"25%"}
-          left={"10%"}
-          ImageUrl={ImageCollection[Id].arr[3]}
-        />
-        <ItemCard
-          reference={ref}
-          top={"65%"}
-          left={"10%"}
+          bottom={window.innerWidth > 1024 ? "5%" : "16%"}
+          left={window.innerWidth > 1024 ? "" : "5%"}
           ImageUrl={ImageCollection[Id].arr[4]}
+          emoji={emoji}
+        />
+
+        <ItemCard
+          reference={ref}
+          bottom={window.innerWidth > 1024 ? "54%" : "71%"}
+          rigth={window.innerWidth > 1024 ? "10%" : "5%"}
+          ImageUrl={ImageCollection[Id].arr[1]}
+          emoji={emoji}
         />
         <ItemCard
           reference={ref}
-          top={"25%"}
-          left={"70%"}
+          bottom={window.innerWidth > 1024 ? "54%" : "44%"}
+          left={window.innerWidth > 1024 ? "10%" : "5%"}
+          ImageUrl={ImageCollection[Id].arr[2]}
+          emoji={emoji}
+        />
+        <ItemCard
+          reference={ref}
+          bottom={window.innerWidth > 1024 ? "16%" : "44%"}
+          rigth={window.innerWidth > 1024 ? "10%" : "5%"}
+          ImageUrl={ImageCollection[Id].arr[3]}
+          emoji={emoji}
+        />
+
+        <ItemCard
+          reference={ref}
+          bottom={window.innerWidth > 1024 ? "16%" : "16%"}
+          left={window.innerWidth > 1024 ? "10%" : "55%"}
           ImageUrl={ImageCollection[Id].arr[5]}
+          emoji={emoji}
         />
       </div>
+      <hr />
     </>
   );
 }
--- src/Components/Home/HomeComponents/DealsPages/ItemCard.jsx
+++ src/Components/Home/HomeComponents/DealsPages/ItemCard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { motion, useAnimation, useInView } from "framer-motion";
 
-function ItemCard({ reference, top, bottom, left, rigth, ImageUrl }) {
+function ItemCard({ reference, top, bottom, left, rigth, ImageUrl, emoji }) {
   const parentControl = useAnimation();
   const innerControls = [useAnimation(), useAnimation(), useAnimation()];
   const isInView = useInView(reference, { amount: 0.6 });
@@ -23,57 +23,57 @@ function ItemCard({ reference, top, bottom, left, rigth, ImageUrl }) {
 
   return (
     <motion.span
-      //   ref={scope}
       variants={{
         visible: {
           top: `${top}`,
           bottom: `${bottom}`,
           left: `${left}`,
           right: `${rigth}`,
+          opacity: 1,
         },
       }}
       animate={parentControl}
-      transition={{ duration: 1, ease: "easeInOut" }}
-      className="h-fit min-w-[20vw] max-w-fit flex justify-center items-center bg-gradient-to-br from-slate-600/60 to-white border border-black rounded-xl absolute"
+      transition={{ duration: 0.6, ease: "easeInOut" }}
+      className="w-[40%] min-h-[22vh] max-h-[22vh] flex justify-center items-center bg-gradient-to-br from-slate-600/60 to-white border border-black rounded-xl absolute lg:min-h-[30vh] lg:max-h-[30vh] lg:min-w-[20vw] lg:max-w-[20vw] "
     >
       <motion.span
         variants={{
-          initial: { bottom: "100px", opacity: 0 },
-          final: { top: 0, opacity: 1, color: "red" },
+          initial: { top: "100px", opacity: 0 },
+          final: { top: "-20px", opacity: 1, color: "red" },
         }}
         initial="initial"
         animate={innerControls[0]}
         transition={{ duration: 1, ease: "circInOut" }}
-        className=" absolute bottom-10 -left-10"
+        className=" absolute -left-4  text-sm font-bold bg-black p-2 px-4 rounded-full flex justify-center items-center"
       >
-        hello
+        {Math.round(Math.random() * (60 - 20) + 20)}% OFF
       </motion.span>
       <motion.span
         variants={{
-          initial: { bottom: "100px", opacity: 0 },
-          final: { top: 0, opacity: 1, color: "red" },
+          initial: { bottom: "-120px", opacity: 0 },
+          final: { bottom: "-8px", opacity: 1, color: "red" },
         }}
         initial="initial"
         animate={innerControls[1]}
         transition={{ duration: 1, ease: "circInOut" }}
-        className=" absolute -bottom-8 left-10"
+        className=" absolute -left-3 text-4xl lg:text-5xl"
       >
-        hello
+        {emoji}
       </motion.span>
       <motion.span
         variants={{
-          initial: { bottom: "100px", opacity: 0 },
+          initial: { top: "100px", opacity: 0 },
           final: { top: 0, opacity: 1, color: "red" },
         }}
         initial="initial"
         animate={innerControls[2]}
         transition={{ duration: 1, ease: "circInOut" }}
-        className=" absolute top-6 -right-10"
+        className=" absolute -right-2 bg-blue-400"
       >
         hello
       </motion.span>
 
-      <img className="h-48 " src={ImageUrl} alt="" />
+      <img className="h-fit lg:h-44 " src={ImageUrl} alt="" />
     </motion.span>
   );
 }
